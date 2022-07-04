f5c733322b3237c648e9fb2010a096021a8728ed
CieloCris <97429651+CieloCris@users.noreply.github.com>
2022-07-03 19:24:55 -0500

    mofications to some files

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,48 +2,57 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 5);
+let map = L.map('mapid').setView([30.2672, -97.7431], 5);
 
-// Coordinates for each point to be used in the polyline.
+// Coordinates for each point to be used in the line.
 let line = [
-    // SFO
+    [33.9416, -118.4085],
     [37.6213, -122.3790],
-    // AUS
-    [30.1975, -97.6664],
-    // YYZ
+    [30.2672, -97.7431],
     [43.6777, -79.6248],
-    // JFK
-    [40.6413, -73.7781]
+    [40.7128, -74.0060]
   ];
 
-// Create a polyline using the line coordinates and make the line black.
+  // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-    color: "blue",
-    dashArray: "10",
-    weight: 4,
-    opacity: .5
- }).addTo(map);
+    color: "blue"
+    
+  }).addTo(map);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
 
 // Get data from cities.js
 let cityData = cities;
 
-// // Loop through the cities array and create one marker for each city.
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+
+
+// We create the tile layer that will be the background of our map.
+// Dark Map code:
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Light Map code:
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Satellite Map code:
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Loop through the cities array and create one marker for each city.
+// Skill Drill 12.4.2 edit the lineweight, add the color orange, radius decreased by 200K
+// and dark map
 // cityData.forEach(function(city) {
 //     console.log(city)
 //     L.circleMarker(city.location, {
-//         radius: city.population/100000,
+//         radius: city.population/200000,
 //         color: 'orange',
-//         fillColor: '#ffffa1'
+//         lineweight: 4
 //     })
 //     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
+//   .addTo(map);
 // });
 
 // Then we add our 'graymap' tile layer to the map.
